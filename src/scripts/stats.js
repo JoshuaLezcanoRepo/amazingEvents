47b9af4c86db9340094d928d250ce32c4e18a017
Joshua David <96133436+JoshuaLezcanoRepo@users.noreply.github.com>
2023-09-11 18:14:05 -0300

    Data de Tablas Añadido

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -1,6 +1,5 @@
 const $tdEventStatisticsBase = document.getElementById('eventStatistics');
-
-$tdEventStatisticsBase.innerHTML = `<td>Metallica</td><td>Metallica</td><td>Metallica</td>`
+const $tdPastStatisticsBase = document.getElementById('pastStatistics');
 
 async function getData() {
     try {
@@ -18,6 +17,126 @@ getData();
 function finalData(data) {
     const events = data.events;
     const curDate = data.currentDate;
+
+    // Obtengo Eventos Futuros y Pasados
+    function filterUpcomingEvents(events, curDate) {
+        const upcomingEvents = [];
+        for (let event of events) {
+            if (Date.parse(curDate) < Date.parse(`${event.date}`)) {
+                upcomingEvents.push(event);
+            }
+        }
+        return upcomingEvents;
+    }
+
+    const createUpcomingEvents = filterUpcomingEvents(events, curDate);
+    // console.log(createUpcomingEvents);
+
+    function filterPastEvents(events, curDate) {
+        const pastEvents = [];
+        for (let event of events) {
+            if (Date.parse(curDate) > Date.parse(`${event.date}`)) {
+                pastEvents.push(event);
+            }
+        }
+        return pastEvents;
+    }
+
+    const createPastEvents = filterPastEvents(events, curDate);
+    // console.log(createPastEvents);
+
+    // Filtro por Categorías (Upcoming & Past)
+    const allCategoriesUpcoming = [...new Set(createUpcomingEvents.map(event => event.category))];
+    console.log(allCategoriesUpcoming);
+
+    const allCategoriesPast = [...new Set(createPastEvents.map(event => event.category))];
+    console.log(allCategoriesPast);
+
+    function highestAssistance(events) {
+        let contador = 0;
+        let eventTitle = "";
+        events.forEach((event) => {
+            let number = ((event.assistance / event.capacity) * 100);
+
+            if (number > contador) {
+                contador = number;
+                eventTitle = event.name;
+            }
+        })
+        return `${eventTitle} ${contador.toFixed(2)}%`;
+    }
+
+    // console.log(highestAssistance(events));
+
+    function lowestAssistance(events) {
+        let contador = 100;
+        let eventTitle = "";
+        events.forEach((event) => {
+            let number = ((event.assistance / event.capacity) * 100);
+
+            if (number < contador) {
+                contador = number;
+                eventTitle = event.name;
+            }
+        })
+        return `${eventTitle} ${contador.toFixed(2)}%`;
+    }
+
+    // console.log(lowestAssistance(events));
+    
+    function largerCapacity(events) {
+        let contador = 0;
+        let eventTitle = "";
+        events.forEach((event) => {
+            if (event.capacity > contador) {
+                contador = event.capacity;
+                eventTitle = event.name;
+            }
+        })
+        return `${eventTitle} ${contador.toLocaleString(undefined, {
+            maximumFractionDigits: 0})}`;
+    }
+
+    // console.log(largerCapacity(events));
+
+    $tdEventStatisticsBase.innerHTML = `<td>${highestAssistance(createPastEvents)}</td><td>${lowestAssistance(createPastEvents)}</td><td>${largerCapacity(events)}</td>`
+
+
+    // Filter Past Events - Category - Revenue
+    function dataPastEvents(categories, events) {
+        let dataFinal = [];
+        categories.forEach((category) => {
+            let eventsCats = events.filter((event) => category == event.category);
+            let eventsRevenues = eventsCats.reduce((contador, event) => contador + event.price * (event.estimate || event.assistance), 0);
+            let attendance = eventsCats.reduce((contador, event) => contador + ((event.assistance || event.estimate) / event.capacity) * 100, 0);
+            dataFinal.push({
+                category,
+                eventsRevenues,
+                attendance: attendance / eventsCats.length,
+            });
+        });
+        return dataFinal;
+    }
+
+    const dataShowPastEvent = dataPastEvents(allCategoriesPast, createPastEvents);
+    const dataShowUpcomingEvent = dataPastEvents(allCategoriesUpcoming, createUpcomingEvents);
+
+    // Función para crear el template de las cards
+    function generateTemplate(event) {
+        return `
+            <tr>${event.category}</tr>`
+    }
+
+    // Función para crear las cards, con parametro (array con los eventos filtrados y la base donde se colocaran las cards)
+    function createCards(dataShowPastEvent, baseUpcoming) {
+        let templateCardsUpcoming = "";
+        dataShowPastEvent.forEach(event => {
+            templateCardsUpcoming += generateTemplate(event);
+        })
+        baseUpcoming.innerHTML = templateCardsUpcoming;
+    }
+
+    createCards(dataShowPastEvent, $tdPastStatisticsBase);
 
     let arrayFav = [];
     function checkFavorite() {
